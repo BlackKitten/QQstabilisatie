0c423d6ebc08696400aa0efde9277f272c52577e
BlackKitten <b.public.griffin@gmail.com>
2015-12-06 14:59:59 +0100

    finished dataset_handler alpha

--- src/DataSet_handler.java
+++ src/DataSet_handler.java
@@ -4,8 +4,9 @@ import org.neuroph.core.data.DataSet;
 
 
 public class DataSet_handler {
+	private DataSet ds;
 	public DataSet_handler(Properties p) {
-		// TODO Auto-generated constructor stub
+		this.ds=new DataSet(12, 4);
 	}
 
 	/**
@@ -14,18 +15,21 @@ public class DataSet_handler {
 	 * @param new_ga
 	 */
 	public void add(double[] old_ga,double[] new_ga, double[] speeds){
-		
+		double[] input=new double[6];
+		System.arraycopy(old_ga, 0, input, 0, old_ga.length);
+		System.arraycopy(new_ga, 0, input, 3, new_ga.length);
+		this.ds.addRow(input, speeds);
 	}
 	
 	public DataSet get(){
-		return new DataSet(6, 4);
+		return this.ds;
 	}
 	
 	public void load_file(String fn){
-		
+		this.ds=DataSet.createFromFile(fn, 12, 6, ";");
 	}
 	
 	public void to_file(String fn){
-		
+		this.ds.save(fn);
 	}
 }
